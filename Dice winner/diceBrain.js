6b9b1a1b428e7aac0ee78a38c60368064515cfcf
NathanAlmaleh <37499295+NathanAlmaleh@users.noreply.github.com>
2020-02-08 17:09:29 +0200

    Dice roll Website
    creating a website with rolling dices

---
+++ Dice winner/diceBrain.js
@@ -0,0 +1,39 @@
+
+var playerOne ,playerTwo;
+
+function randomNumber(){
+
+    playerOne = Math.floor(Math.random() * 6) + 1;
+    
+    playerTwo =  Math.floor(Math.random() * 6) + 1;
+
+    console.log(playerOne +" " + playerTwo);
+
+    playerOnePictuer(playerOne)
+    playerTwoPictuer(playerTwo);
+
+    displayWinner();
+}
+
+function playerOnePictuer(number){
+    document.getElementById("player1pic").src = "pic/dice"+number+".png";
+
+}
+
+function playerTwoPictuer(number){
+    document.getElementById("player2pic").src = "pic/dice"+number+".png";
+}
+
+function displayWinner(){
+    var stringWinner;
+    if(playerOne>playerTwo){
+        stringWinner = "Player 1";
+    }
+    else if(playerOne<playerTwo){   
+        stringWinner = "Player 2";
+    }
+    else{
+        stringWinner = "Its a Tie !!";
+    }
+    document.getElementById("Winner").textContent = "The winner: "+stringWinner;
+}
